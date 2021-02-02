0894b8a69fdfbef2186c231806a32f2c9952a0fd
Alessandro <internicola.alessandro@gmail.com>
2021-02-02 12:49:00 +0100

    Init
    Inizializzazione esercizio

---
+++ Versione1/index.php
@@ -0,0 +1,60 @@
+<!-- Stampiamo i dischi solo con l’utilizzo di PHP, che stampa direttamente i dischi in pagina: al caricamento della pagina ci saranno tutti i dischi. -->
+
+<?php
+
+$db = [
+  "album1" => [
+    "title" => "New Jersey",
+    "artist" => "Bon Jovi",
+    "year" => 1988,
+    "cover" => "immmmaggine mbo?"
+  ],
+  "album1" => [
+    "title" => "Live at Wembley",
+    "artist" => "Queen",
+    "year" => 1992,
+    "cover" => "immmmaggine mbo?"
+  ],
+  "album1" => [
+    "title" => "New Jersey",
+    "artist" => "Bon Jovi",
+    "year" => 2018,
+    "cover" => "immmmaggine mbo?"
+  ],
+  "album1" => [
+    "title" => "New Jersey",
+    "artist" => "Bon Jovi",
+    "year" => 2018,
+    "cover" => "immmmaggine mbo?"
+  ],
+  "album1" => [
+    "title" => "New Jersey",
+    "artist" => "Bon Jovi",
+    "year" => 2018,
+    "cover" => "immmmaggine mbo?"
+  ],
+  "album1" => [
+    "title" => "New Jersey",
+    "artist" => "Bon Jovi",
+    "year" => 2018,
+    "cover" => "immmmaggine mbo?"
+  ],
+];
+
+ ?>
+
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Ajax dischi php ver. 1</title>
+  </head>
+
+  <body>
+
+
+
+  </body>
+
+</html>
---
+++ Versione2/index.php
@@ -0,0 +1,26 @@
+<!-- Seconda Milestone:
+Attraverso l’utilizzo di AJAX: al caricamento della pagina ajax chiederà attraverso una chiamata i dischi a php e li stamperà attraverso VueJS. -->
+
+
+
+<?php
+
+
+
+ ?>
+
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+
+  <body>
+
+
+
+  </body>
+
+</html>
